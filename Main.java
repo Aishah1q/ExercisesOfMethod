18b99a6ae27c69043e0697a5cc82acd7c93c8b45
Aishah1q <148686153+Aishah1q@users.noreply.github.com>
2023-10-30 14:48:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+ //1
+      /*  System.out.println("Enter First Number");
+        int num1 = input.nextInt();
+        System.out.println("Enter Second Number");
+        int num2 = input.nextInt();
+        System.out.println("Enter third Number");
+        int num3 = input.nextInt();
+
+         int N=check(num1,num2,num3);
+         System.out.println("The smallest value is :"+N);*/
+//2
+      /*  System.out.println("Enter First Number");
+        int number = input.nextInt();
+         check1(number);*/
+//3
+       /* System.out.println("Enter Password");
+        String pass=input.next();
+        if(Password(pass)){
+            System.out.println("is valid");
+        }
+        else
+            System.out.println("Not a valid");*/
+
+
+
+
+
+    }
+    //1
+   /* public  static int check(int num ,int num1,int num2){
+        if(num<num1&&num<num2){
+            return num;
+        }
+        else if(num1<num&&num1<num2){
+            return num1;}
+        else
+            return num2;}*/
+
+    //2
+  /*  public  static void check1(int N){
+
+        if(N>0)
+            System.out.println("the number is Positive");
+        else if (N<0){
+            System.out.println("the number is Negative");
+        }
+        else
+            System.out.println("the number is Zero");
+    }*/
+
+    //3
+   /* public  static boolean Password(String Pass) {
+        if (Pass.length()>8) {
+            return true;
+        }
+    else {
+        return false;
+        }
+        }*/
+
+
+
+}
+
+
+
